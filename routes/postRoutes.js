ed1aba519f08ebc23835e7cf07e20f9ac8a51204
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-05-11 15:31:26 +0530

    s

--- routes/postRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createPost } from "../controllers/postController.js";
-const Router = express.Router();
-
-Router.route("/").post(createPost);
-
-export default Router;
